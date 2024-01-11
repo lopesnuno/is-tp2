06cddd9a1ce705239c7cd2a4de9349092a742afb
lantoniomiranda <lantoniomiranda@ipvc.pt>
2024-01-11 19:34:46 +0000

    rel schema updated

--- docker/images/db/rel/initial_schema.sql
+++ docker/images/db/rel/initial_schema.sql
@@ -9,6 +9,13 @@ CREATE TABLE public.teams (
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE public.college (
+	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name            VARCHAR(250) NOT NULL,
+	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
 CREATE TABLE public.countries (
 	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	name            VARCHAR(250) UNIQUE NOT NULL,
@@ -23,9 +30,47 @@ CREATE TABLE public.players (
 	age             INT NOT NULL,
 	team_id         uuid,
 	country_id      uuid NOT NULL,
+    college_id      uuid,
+    height          INT NOT NULL,
+    weight          INT NOT NULL,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE public.draft (
+    id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    year            INT NOT NULL,
+    round           INT NOT NULL,
+    number          INT NOT NULL,
+    player_id       uuid NOT NULL
+)
+
+CREATE TABLE public.seasons (
+    id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    year            varchar(50) NOT NULL UNIQUE,
+)
+
+CREATE TABLE public.season_player (
+    id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    season_id          uuid NOT NULL,
+    player_id          uuid NOT NULL
+)
+
+CREATE TABLE public.stats (
+    id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    season_player   uuid NOT NULL,
+    gp              FLOAT,
+    pts             FLOAT,
+    reb             FLOAT,
+    ast             FLOAT,
+    net_rating      FLOAT,
+    oreb_pct        FLOAT,
+    dreb_pct        FLOAT,
+    usg_pct         FLOAT,
+    ts_pct          FLOAT,
+    ast_pct         FLOAT,
+)
+
 
 ALTER TABLE players
     ADD CONSTRAINT players_countries_id_fk
@@ -37,14 +82,29 @@ ALTER TABLE players
         FOREIGN KEY (team_id) REFERENCES teams
             ON DELETE SET NULL;
 
-/* Sample table and data that we can insert once the database is created for the first time */
-CREATE TABLE public.teachers (
-	name    VARCHAR (100),
-	city    VARCHAR(100),
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
+ALTER TABLE players
+    ADD CONSTRAINT players_college_id_fk
+        FOREIGN KEY (college_id) REFERENCES colleges
+            ON DELETE SET NULL;
 
-INSERT INTO teachers(name, city) VALUES('Luís Teófilo', 'Porto');
-INSERT INTO teachers(name, city) VALUES('Ricardo Castro', 'Braga');
+ALTER TABLE draft
+    ADD CONSTRAINT draft_player_id_fk
+        FOREIGN KEY (player_id) REFERENCES players
+            ON DELETE SET NULL;
+
+ALTER TABLE season_player
+    ADD CONSTRAINT season_player_fk
+        FOREIGN KEY (player_id) REFERENCES players
+            ON DELETE SET NULL;
+
+ALTER TABLE season_player
+    ADD CONSTRAINT season_player_fk
+        FOREIGN KEY (season_id) REFERENCES seasons
+            ON DELETE SET NULL;
+
+ALTER TABLE stats
+    ADD CONSTRAINT stats_season_player_id_fk
+        FOREIGN KEY (id) REFERENCES season_player
+            ON DELETE SET NULL;
+
 
