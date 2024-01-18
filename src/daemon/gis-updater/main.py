a8583a4a54d3d54958759def3d2a0d2c2006b6f1
lantoniomiranda <lantoniomiranda@ipvc.pt>
2024-01-18 13:12:31 +0000

    get coordinates working

--- src/daemon/gis-updater/main.py
+++ src/daemon/gis-updater/main.py
@@ -8,10 +8,11 @@ POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 60
 ENTITIES_PER_ITERATION = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 XML_PATH = "/xml"
 
+geolocator = Nominatim(user_agent="is-tp2")
 
-def get_colleges(xml_path, iterations=None):
+def get_colleges(xml_path, max_iterations):
     college_names = set()
-    iterations = 0
+
     for file in os.listdir(xml_path):
         if file.endswith(".xml"):
             file_path = os.path.join(xml_path, file)
@@ -27,14 +28,24 @@ def get_colleges(xml_path, iterations=None):
     return list(college_names)
 
 
+def get_coordinates_by_college_name(name):
+    location = geolocator.geocode(name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None
+
+
 if __name__ == "__main__":
 
     while True:
         print(f"Getting up to {ENTITIES_PER_ITERATION} entities without coordinates...")
         # !TODO: 1- Use api-gis to retrieve a fixed amount of entities without coordinates (e.g. 100 entities per iteration, use ENTITIES_PER_ITERATION)
-        colleges = get_colleges(XML_PATH)
+        colleges = get_colleges(XML_PATH, ENTITIES_PER_ITERATION)
         # !TODO: 2- Use the entity information to retrieve coordinates from an external API
+        for college in colleges:
+            geo = get_coordinates_by_college_name(college)
+            print(college, ' -> ', geo)
         # !TODO: 3- Submit the changes
-        # time.sleep(POLLING_FREQ)
-        print(colleges)
+
 
