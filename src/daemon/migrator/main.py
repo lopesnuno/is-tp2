201b3d6fe351efebecd0e421c217a591e55fc666
lantoniomiranda <lantoniomiranda@ipvc.pt>
2024-01-20 11:37:24 +0000

    retrieving colleges

--- src/daemon/migrator/main.py
+++ src/daemon/migrator/main.py
@@ -1,11 +1,20 @@
 import sys
 import time
-
 import psycopg2
 from psycopg2 import OperationalError
+import xml.etree.ElementTree as ET
 
-POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 60
+POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 120
 
+
+def check_updates(cursor, last_check):
+    query = f"SELECT * FROM imported_documents WHERE updated_on > %s OR created_on > %s;"
+    cursor.execute(query, (last_check, last_check))
+    data = cursor.fetchall()
+    if data:
+        return data
+    else:
+        return None
 
 def print_psycopg2_exception(ex):
     # get details about the exception
@@ -43,17 +52,51 @@ if __name__ == "__main__":
         except OperationalError as err:
             print_psycopg2_exception(err)
 
-        if db_dst is None or db_org is None:
-            continue
+        if db_org is None or db_dst is None:
+            sys.exit("Failed to connect to the database.")
 
-        print("Checking updates...")
         # !TODO: 1- Execute a SELECT query to check for any changes on the table
-        # !TODO: 2- Execute a SELECT queries with xpath to retrieve the data we want to store in the relational db
+
+        last_check = '2024-01-19 00:00:00'
+
+        try:
+            with db_org.cursor() as cursor:
+                while last_check:
+                    results = check_updates(cursor, last_check)
+                    if results is not None:
+                        unique_colleges = set()
+                        for row in results:
+                            xml_data = row[2]
+                            try:
+                                root = ET.fromstring(xml_data)
+                                colleges = [college.find("College/name").text for college in root.findall(".//Player")]
+                                # !TODO: 2 - Retreive info that will be added in db-rel
+                                for name in colleges:
+                                    if name is not None:
+                                        if name not in unique_colleges:
+                                            unique_colleges.add(name)
+
+
+                            except ET.ParseError as e:
+                                print("Error parsing XML data:", e)
+
+                        time.sleep(1)
+
+                    else:
+                        print('No results.')
+
+                    last_check = time.strftime('%Y-%m-%d %H:%M:%S')
+                    time.sleep(POLLING_FREQ)
+
+        except OperationalError as err:
+            print_psycopg2_exception(err)
+        finally:
+            db_org.close()
+
+
         # !TODO: 3- Execute INSERT queries in the destination db
         # !TODO: 4- Make sure we store somehow in the origin database that certain records were already migrated.
         #          Change the db structure if needed.
 
         db_org.close()
-        db_dst.close()
-
-        time.sleep(POLLING_FREQ)
+        # db_dst.close()
