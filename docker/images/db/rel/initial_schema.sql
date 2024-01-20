3bba7b8e1f20706e5c7ff00d7b301b0473d937d3
lantoniomiranda <lantoniomiranda@ipvc.pt>
2024-01-20 11:37:07 +0000

    changed db-rel schema

--- docker/images/db/rel/initial_schema.sql
+++ docker/images/db/rel/initial_schema.sql
@@ -2,13 +2,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS POSTGIS;
 CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;
 
-CREATE TABLE public.teams (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) NOT NULL,
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
 CREATE TABLE public.colleges (
 	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	name            VARCHAR(250) NOT NULL,
@@ -19,32 +12,19 @@ CREATE TABLE public.colleges (
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE public.countries (
-	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-	name            VARCHAR(250) UNIQUE NOT NULL,
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
 CREATE TABLE public.players (
 	id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	name            VARCHAR(250) NOT NULL,
 	age             INT NOT NULL,
-	team_id         uuid,
-	country_id      uuid NOT NULL,
+	country         VARCHAR(250),
     college_id      uuid,
     height          INT NOT NULL,
     weight          INT NOT NULL,
+    draft_year      INT,
+    draft_round     INT,
+    draft_number    INT,
 	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE public.draft (
-    id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    year            INT NOT NULL,
-    round           INT NOT NULL,
-    number          INT NOT NULL,
-    player_id       uuid NOT NULL
 );
 
 CREATE TABLE public.seasons (
@@ -75,23 +55,8 @@ CREATE TABLE public.stats (
 
 
 ALTER TABLE players
-    ADD CONSTRAINT players_countries_id_fk
-        FOREIGN KEY (country_id) REFERENCES countries
-            ON DELETE CASCADE;
-
-ALTER TABLE players
-    ADD CONSTRAINT players_teams_id_fk
-        FOREIGN KEY (team_id) REFERENCES teams
-            ON DELETE SET NULL;
-
-ALTER TABLE players
     ADD CONSTRAINT players_college_id_fk
         FOREIGN KEY (college_id) REFERENCES colleges
-            ON DELETE SET NULL;
-
-ALTER TABLE draft
-    ADD CONSTRAINT draft_player_id_fk
-        FOREIGN KEY (player_id) REFERENCES players
             ON DELETE SET NULL;
 
 ALTER TABLE season_player
